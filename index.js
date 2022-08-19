72809590b322a1e22f23560fa87bfb2a0c9090f1
EstherWanjiruUge <ugeesther@gmail.com>
2022-08-19 11:32:29 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(records) {
+  const foundObj = records.find((record) => {
+    return record["result"] === "W";
+  });
+  if (foundObj !== undefined) {
+    return foundObj["year"];
+  }
+}
